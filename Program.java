13e74317c3e22eae7715faa410aa6e0b8faeae75
MadMadeMadelyn <madelynching916@gmail.com>
2024-09-03 16:07:33 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,36 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
+public class Program7 {
+    public static void main(String[]args){
+        //20 klevins = schrute-buck
+        //12 stanley-nickels = klevin
+       
+        //100 stanley-nickels = schrute-buck
+        int schruteBucks;
+        int klevins;
+        int stanleyNickels;
+       
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Enter schrute-bucks: ");
+        schruteBucks = numScanner.nextDouble();
+        
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Enter klevins: ");
+        klevins = numScanner.nextDouble();
+        
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Enter stanley-nickels: ");
+        stanleyNickels = numScanner.nextDouble();
+    
+       kelvinNickel = kelvin * 12;
+       kelvinNickel = stanleyNickel + kelvinNickel
+       kelvinNickel = 100 / kelvinNickel
+       
+        System.out.println("Decimal schrute-bucks = $" schruteBucks + "." + kelvinNickel);
+    }
+}
+       
 
 //Paste console output below:
 /*
